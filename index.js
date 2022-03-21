7a468ffe55575b551ab5b37552a4cb8794b6516d
Donal-R77 <rakotoarisoaromarick@gmail.com>
2022-03-21 12:23:19 +0300

    modif3

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ console.log('ito indray ary ee!!!!');
 let tab = [5,6,7,8]
 const pair = tab.find(num => num%2 == 0);
 console.log(pair);
+
+(function(){
+   console.log('les valeurs pairs :'+[1 , 5 , 6 , 8].find(x =>x%2));
+})();
